5830ac336d7ff4bce418828057ad04babde7fec7
JoseLuisAlvarezManica <114442646+JoseLuisAlvarezManica@users.noreply.github.com>
2026-01-28 10:06:30 -0600

    Implementar base de datos

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 from flask import Flask, render_template
+from db import get_db, create_database
+
 
 app = Flask(__name__)
+
+create_database()
 
 @app.route('/')
 def home():
     # Renderiza el archivo index.html ubicado en templates/
     return render_template('index.html')
+
+# Login (GET/POST) redirigir a login si no funciona
+
+# Logout
+
+#Crud completo de productos
+# /productos (GET)
+
+# /productos/nuevo (POST)
+
+# /productos/<id>/editar (POST)
+
+# /productos/<id>/eliminar (DELETE)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ db.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+SQUEMA_SQL = '''
+    CREATE TABLE IF NOT EXISTS stocks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT NOT NULL,
+        precio REAL CHECK (precio >= 0),
+        stock INTEGER CHECK (stock >= 0),
+        activo INTEGER DEFAULT 0
+    ); '''
+
+def get_db():
+    conn = sqlite3.connect('productos.db')
+    return conn
+
+def create_database():
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute(SQUEMA_SQL)
+    conn.commit()
+    conn.close()
+
